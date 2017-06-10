f1de61de0db4061db4b8169be50a26e5ca3328f5
hudsonbl <hudsonbl@oregonstate.edu>
2017-06-10 16:31:41 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,18 @@
-document.getElementsByClassName("fa fa-search").addEventListener("click",function(){
 
-});
+var movieTitleButtons = document.getElementsByClassName('tag-buttons');
+var titleDescriptionElem = document.getElementsByClassName('movie-title-display');
+
+
+function moveToBio(event){
+  var titleName = event.currentTarget.textContent;
+  titleDescriptionElem.textContent = titleName;
+  console.log("//==== Title: ",titleName);
+}
+
+for(var i = 0; i < titleDescriptionElem.length; i++){
+  titleDescriptionElem[i].addEventListener('click', moveToBio);
+  console.log(i);
+}
+
+
+console.log("hello");
