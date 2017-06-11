3748b69c5011b0aaf403ffe997350ebe5080087a
hudsonbl <hudsonbl@oregonstate.edu>
2017-06-11 02:35:54 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,27 +1,41 @@
 var path = require('path');
 var fs = require('fs');
 var express = require('express');
-var exphbs = requrie('express-handlebars');
+var exphbs = require('express-handlebars');
 
-var moveTitle = require('./movieTitles.json');
+var movieTitles = require('./movieTitles.json');
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get('/', function(req, res, next){
 	console.log("///==Directory==///");
+
+	var templateArgs = {
+		name: movieTitles.Movies.name,
+		review: movieTitles.Movies.review,
+		rating: movieTitles.Movies.rating
+	};
+
+	var start = false;
+	res.render('moviePage', {list:templateArgs, start:start});
 	next();
 });
 
-app.get('/:titles', function(req, res, next){
+app.get('/movies/:titles', function(req, res, next){
 	var titles = req.params.titles;
 	console.log("///==" + titles.name + "==///");;
 	next();
 });
 
-app.use(express.static(path.join(__dirnam, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//app.get('*', function(req, res));
+app.get('*', function(req, res){
+	res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
 
 app.listen(port, function(){
-	console.log("///=== Server Listening: " + port);
-}
+	console.log("///=== Server Listening: ", port);
+});
